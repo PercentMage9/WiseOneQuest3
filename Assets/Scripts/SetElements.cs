ef10e28645bf1d6b6c39b8d4aeac2027dba43c2b
PercentMage9 <PercentMage09@gmail.com>
2023-05-02 15:08:28 +0100

    Tried adding but broken
    Fix the dropdown menu

--- Assets/Scripts/SetElements.cs
+++ Assets/Scripts/SetElements.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class SetElements : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Note: elements go in order of: 0 - fire, 1 - water, 2 - earth, 3 - air
+    //Element values were set as strings to act nicer with the drop down menu
+
+    public string playerElementValue;
+    public string enemyElementValue;
+    public GameObject elementList;
+
     void Start()
     {
-        
+        var RandNumber = Random.Range(0,3);             //Pick a random number between 0 and 3
+        enemyElementValue = RandNumber.ToString();                 //Set enemy element value to that number as a string
     }
 
-    // Update is called once per frame
-    void Update()
+    //For setting the player's element depending on what element is selected
+    public void setPlayerElement()
     {
-        
+       // playerElementValue = elementList.GetComponent<TMPro.TextMeshProUGUI>().m_Value;
+       playerElementValue = TMPro.TMP_Dropdown[] elementList = GetComponentsInChildren<TMPro.TMP_Dropdown>().value;
     }
 }
+
