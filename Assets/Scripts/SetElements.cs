5d246f282691254661712710d92c01664ea85d64
Ava <PercentMage09@gmail.com>
2023-05-02 18:39:47 +0100

    Added Scenery
    Fixed issues with the element entering system
    Added some aswesome poggers scenery

--- Assets/Scripts/SetElements.cs
+++ Assets/Scripts/SetElements.cs
@@ -1,27 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SetElements : MonoBehaviour
 {
     //Note: elements go in order of: 0 - fire, 1 - water, 2 - earth, 3 - air
-    //Element values were set as strings to act nicer with the drop down menu
 
-    public string playerElementValue;
-    public string enemyElementValue;
-    public GameObject elementList;
+    public int playerElementValue;
+    public int enemyElementValue;
+    public TMP_Dropdown elementList;
 
     void Start()
     {
         var RandNumber = Random.Range(0,3);             //Pick a random number between 0 and 3
-        enemyElementValue = RandNumber.ToString();                 //Set enemy element value to that number as a string
+        enemyElementValue = RandNumber;                 //Set enemy element value to that number
     }
 
     //For setting the player's element depending on what element is selected
     public void setPlayerElement()
     {
-       // playerElementValue = elementList.GetComponent<TMPro.TextMeshProUGUI>().m_Value;
-       playerElementValue = TMPro.TMP_Dropdown[] elementList = GetComponentsInChildren<TMPro.TMP_Dropdown>().value;
+        playerElementValue = elementList.value;
     }
 }
-
